aa55b8aa0e0b2feef0be6be81066111e370833ee
魏壮志 <weizhuangzhi@qq.com>
2022-11-14 16:02:17 +0800

    初始化asp.net core 7项目

---
+++ src/Erocten.Website/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Erocten.Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
