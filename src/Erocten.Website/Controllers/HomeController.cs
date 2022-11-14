f2a6890c5ef10b269449fba0f6e3e57162518491
魏壮志 <weizhuangzhi@qq.com>
2022-11-14 16:15:18 +0800

    设置全局命名空间及文件范围的命名空间声明

--- src/Erocten.Website/Controllers/HomeController.cs
+++ src/Erocten.Website/Controllers/HomeController.cs
@@ -1,32 +1,27 @@
-﻿using System.Diagnostics;
-using Erocten.Website.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace Erocten.Website.Controllers;
 
-namespace Erocten.Website.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    public IActionResult Index()
+    {
+        return View();
+    }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
