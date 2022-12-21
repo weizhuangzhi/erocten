03ffff662b3ef15599cee1c217159ff9b0c4baf9
魏壮志 <weizhuangzhi@qq.com>
2022-12-21 16:43:34 +0800

    添加约定

--- src/Erocten.Foundation.EntityFrameworkCore/BaseDbContext.cs
+++ src/Erocten.Foundation.EntityFrameworkCore/BaseDbContext.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Erocten.Foundation.EntityFrameworkCore.Conventions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Erocten.Foundation.EntityFrameworkCore;
 
 public class BaseDbContext : DbContext
 {
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        configurationBuilder.Conventions.Add(_ => new DiscriminatorLengthConvention());
+    }
 }
